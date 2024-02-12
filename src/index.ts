97abc3ea379d1c65b088fc3a7e52ca46b2ba9ee9
Alexei <avenge.now.r@mail.ru>
2024-02-12 22:24:03 +0300

    feat: add handling for server error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import http from 'http';
 import { parse } from 'url';
-import { User } from './types';
+import { ReqType, ResType, User } from './types';
 import chooseEndpoint from './controller';
 
 config();
@@ -16,4 +16,9 @@ const server = http.createServer((req, res) => {
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+server.on('error', (err: Error, req: ReqType, res: ResType) => {
+  res.writeHead(500, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({ message: 'Technical work is underway, come back later' }));
 });
